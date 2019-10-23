29794435a678f8532b23cb6d7fb1c791265ddd65
PPCCCSCS <46571983+PPCCCSCS@users.noreply.github.com>
2019-10-23 10:16:16 -0600

    Version 2
    Shifted as much of the source to functions as possible.
    
    Currently:
    * parses /etc/wpa_supplicant/wpa_supplicant.conf for SSIDs, PSKs, Encryption, and Hidden flags
    * Builds a qr code for the currently connect WLAN
    * Writes qr code, with human-readable SSID and PSK, to a Waveshare epd2in7 compatible screen

--- rok.py
+++ rok.py
@@ -3,128 +3,132 @@
 import sys
 import os
 import subprocess
+import logging
 
-picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
 libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
-ssid = subprocess.getoutput('iwgetid -r')
-wifipw = ""
-encrpt = ""
-hidden = ""
-keyChain = []
-keyIndex = -1
-
-if ssid == "":
-    ssid = "<NONE>"
-    
+  
 if os.path.exists(libdir):
     sys.path.append(libdir)
 
-import logging
 from waveshare_epd import epd2in7
 import time
 from PIL import Image,ImageDraw,ImageFont
 #import traceback
 
-logging.basicConfig(level=logging.DEBUG)
-
-try:
-    logging.info("RPi QR Code WLAN SSID Display")
-            
-    wpas = open("/etc/wpa_supplicant/wpa_supplicant.conf","r")
-    logging.info("Opened wpa_supplicant")
-    
-    line = wpas.readline()
-    logging.info("read first line: " + line)
-    
+def readNetAuths(inFile):
+    keyChain = []
+    keyIndex = -1
+         
+    line = inFile.readline()
+        
     while line != "":
         # Check for network={
         if "network" in line and "=" in line and "{" in line:
         # Create a new list entry
-            logging.info("Found New Network Configuration")
             keyIndex = keyIndex + 1
             keyChain.append(["","","",""])
             while "}" not in line:
-                line = wpas.readline()
+                line = inFile.readline()
         #  Check for ssid="
                 if "ssid=" in line:
-                    logging.info("Found SSID: " + line.split('"')[1])
                     keyChain[keyIndex][0] = line.split('"')[1]
                 if "psk=" in line:
-                    logging.info("Found PreShared Key: " + line.split('"')[1])
                     keyChain[keyIndex][1] = line.split('"')[1]
                 if "key_mgmt=" in line:
                     line = line.strip()
-                    logging.info("Found Key Management: " + line.split('=')[1])
                     keyChain[keyIndex][2] = line.split('=')[1]
                 if "disabled=" in line:
                     line = line.strip()
-                    logging.info("WLAN disabled = " + line.split('=')[1])
                     keyChain[keyIndex][3] = line.split('=')[1]
-        line = wpas.readline()
-        logging.info("Read next line: " + line)
-    print(keyChain)
+        line = inFile.readline()
+    return keyChain
 
+def getNetAuth(ssid, keyChain):
+    
+    print(keyChain)
+    
+    netAuth = ["","","",""]
+    
     for network in keyChain:
         if network[0] == ssid:
-            wifipw = network[1]
+            netAuth[0] = network[0]
+            netAuth[1] = network[1]
             if "WPA" in network[2]:
-                encrpt = "WPA"
+                netAuth[2] = "WPA"
             elif "WEP" in network[2]:
-                encrpt = "WEP"
+                netAuth[2] = "WEP"
             else:
-                encrpt = ""
+                netAuth[2] = ""
             if network[3] == "1":
-                hidden = "true"
+                netAuth[3] = "true"
             else:
-                hidden = "false"
-   
+                netAuth[3] = "false"
+    
+    return netAuth
+
+def qrGen(entry, path):
+    subprocess.call(["/usr/bin/qrencode","-l","L","-m","4","-o",os.path.join(path, entry[0]+".png"),"WIFI:S:"+entry[0]+";T:"+entry[2]+";P:"+entry[1]+";H:"+entry[3]+";"])
+    subprocess.call(["/usr/bin/convert",os.path.join(path, entry[0]+".png"),"-alpha","Off","-resize","176x176","-depth","1",os.path.join(path, entry[0]+".bmp")])
+ 
+def drawScreen(entry, path):
     epd = epd2in7.EPD()
-        
-    logging.info("init and Clear")
+    
     epd.init()
     epd.Clear(0xFF)
     
     centerW = epd.width/2
     centerH = epd.height/2
+    
+    font24 = ImageFont.truetype(os.path.join(path, 'Font.ttc'), 24)
+    font18 = ImageFont.truetype(os.path.join(path, 'Font.ttc'), 18)
+    font35 = ImageFont.truetype(os.path.join(path, 'Font.ttc'), 35)
+    
+    if entry[0] == "":
+        entry[0] = "<NONE>"
+    
+    if entry[1] == "":
+        entry[1] = "<NONE>"
         
-    font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
-    font18 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18)
-    font35 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 35)
-    
-    ## BLOCK BELOW IS VERY *NOT* FINAL CODE!
-   
-    subprocess.call(["/usr/bin/qrencode","-l","L","-m","4","-o",os.path.join(picdir, ssid+".png"),"WIFI:S:"+ssid+";T:"+encrpt+";P:"+wifipw+";H:"+hidden+";"])
-    subprocess.call(["/usr/bin/convert",os.path.join(picdir, ssid+".png"),"-alpha","Off","-resize","176x176","-depth","1",os.path.join(picdir, ssid+".bmp")])
-    
-    if wifipw == "":
-        wifipw = "<NONE>"
-  
-    logging.info("4.read bmp file on window")
-    Himage = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
-    bmp = Image.open(os.path.join(picdir, ssid+".bmp"))
-    Himage.paste(bmp, (0,0))
-    draw = ImageDraw.Draw(Himage)
+    display = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
+    bmp = Image.open(os.path.join(path, entry[0]+".bmp"))
+    display.paste(bmp, (0,0))
+    draw = ImageDraw.Draw(display)
     width, height = draw.textsize("SSID", font=font18)
     draw.text((centerW-(width/2), 170), "SSID", font = font18, fill = 0)
-    width, height = draw.textsize(ssid, font=font18)
-    draw.text((centerW-(width/2), 190), ssid, font = font18, fill = 0)
+    width, height = draw.textsize(entry[0], font=font18)
+    draw.text((centerW-(width/2), 190), str(entry[0]), font = font18, fill = 0)
     width, height = draw.textsize("PASSWORD", font=font18)
     draw.text((centerW-(width/2), 210), "PASSWORD", font = font18, fill = 0)
-    width, height = draw.textsize(wifipw, font=font18)
-    draw.text((centerW-(width/2), 230), wifipw, font = font18, fill = 0)
-    epd.display(epd.getbuffer(Himage))
+    width, height = draw.textsize(entry[1], font=font18)
+    draw.text((centerW-(width/2), 230), entry[1], font = font18, fill = 0)
+    epd.display(epd.getbuffer(display))
     time.sleep(2)
 
-#    logging.info("Clear...")
-#    epd.Clear(0xFF)
-    
-    logging.info("Goto Sleep...")
     epd.sleep()
-    
-except IOError as e:
-    logging.info(e)
-    
-except KeyboardInterrupt:    
-    logging.info("ctrl + c:")
-    epd2in7.epdconfig.module_exit()
-    exit()
+
+
+def main():
+    try:
+        picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
+        
+        wpas = open("/etc/wpa_supplicant/wpa_supplicant.conf","r")
+        
+        netAuths = readNetAuths(wpas)
+        
+        ssid = subprocess.getoutput('iwgetid -r')
+
+        lanKey = getNetAuth(ssid, netAuths).copy()
+       
+        qrGen(lanKey, picdir)
+         
+        drawScreen(lanKey, picdir)
+        
+    except IOError as e:
+        logging.info(e)
+        
+    except KeyboardInterrupt:    
+        logging.info("ctrl + c:")
+        epd2in7.epdconfig.module_exit()
+        exit()
+
+main()
